7af8b7a6274903bcefc737f12662816b55cdfb72
Elliot Chalfant <null>
2014-01-13 19:48:11 -0800

    module added

--- module Tree.rb
+++ module Tree.rb
@@ -1,25 +1,64 @@
+class NoFruitError < StandardError
+end
+
 module Tree
-  @age
-  def age!
-    @age+=1
-  end
+  attr_reader :age, :height, :dead
+  @max_age = 30
+  @@growth_rate = 2
+  @@crop_size = 12
+  @@fruit_bearing_age = 5
+  @@peak_age_min = 13
+  @@peak_age_max = 23
+  @@peak_crop_size = 8
+  @@fruit = "fruit"
 
-  def age_faster!
-    @age*=2
-  end
-end
-
-class OrangeTree
-  include Tree
-  attr_reader :age
-  def initialize
+  def initialize(*args)
     @age = 0
+    @height = 0
+    @dead = false
+    @number_of_fruit = 0
+  end
+
+  def self.change_max_age(num)
+    @max_age = num
+  end
+
+  def self.max_age
+    @max_age
+  end
+
+  def max_age
+    @max_age
+  end
+
+  # Ages the tree one year
+  def age!
+    @age < @max_age ? @age+=1 : @dead = true
+    @height+=2 unless @height==20
+    grow_fruit
+  end
+
+  def dead?
+    @dead
+  end
+
+  def any_fruit?
+    @number_of_fruit > 0
+  end
+
+  def pick_a_fruit!
+    raise NoFruitError, "You don't have any #{@fruit}!" unless self.any_fruit?
+    @number_of_fruit -= 1
+    @fruitclass.new
+  end
+
+  private
+
+  def grow_fruit
+    if @age.between?(@@fruit_bearing_age,@@peak_age_min) || @age.between?(@@peak_age_max,@max_age)
+      @number_of_fruit += @@crop_size
+    elsif @age.between?(13,22)
+      @number_of_fruit += @@peak_crop_size
+    end
   end
 end
-
-tree = OrangeTree.new
-tree.age!
-tree.age!
-p tree.age
-tree.age_faster!
-p tree.age
